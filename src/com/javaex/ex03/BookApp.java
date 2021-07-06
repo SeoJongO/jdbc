c36e1fac6c0397d7a3d26225f6cf160083e0fcdf
SeoJongO <eve7884dbqls@naver.com>
2021-07-06 09:30:48 +0900

    book

---
+++ src/com/javaex/ex03/BookApp.java
@@ -0,0 +1,62 @@
+package com.javaex.ex03;
+
+import java.util.List;
+
+public class BookApp {
+
+	public static void main(String[] args) {
+		
+		// 작가테이블 책테이블 완성
+		// 작가테이블 시퀀스, 책테이블 시퀀스 완성
+		
+		// authorDao.authorInsert(); <-- 작가 6명 추가
+		// bookDao.bookInsert(); <-- 책 6권 추가
+		
+		// 등록 수정 삭제 출력
+		BookDao bookDao = new BookDao();
+		List<BookVo> BookList;
+
+		BookList = bookDao.BookList();
+		printList(BookList);
+
+		// 작가등록
+		BookVo iBookVo = new BookVo("삼국지","마로니에","2002-06-07", 1);
+		int iCount = bookDao.bookInsert(iBookVo);
+		if (iCount > 0) {
+			System.out.println("[등록되었습니다.]");
+		} else {
+			System.out.println("[관리자에게 문의하세요(" + iCount + ")]");
+		}
+
+		// 작가수정
+		int uCount = bookDao.bookUpdate("노인과바다","몰라","2021-07-05",8);
+
+		// 작가삭제
+		int dCount = bookDao.bookDelete(9);
+		
+		// 검색
+
+
+	}
+
+	// 리스트 출력 메소드
+	public static void printList(List<BookVo> bookList) {
+
+		for (int i = 0; i < bookList.size(); i++) {
+
+			BookVo bookVo = bookList.get(i);
+			System.out.println(bookVo.getBook_id()
+					   + "," + bookVo.getTitle()
+					   + "," + bookVo.getPubs()
+					   + "," + bookVo.getPub_date()
+					   + "," + bookVo.getAuthor_id()
+					   + "," + bookVo.getAuthor_name()
+					   + "," + bookVo.getAuthor_desc());
+		}
+
+		System.out.println("=================================");
+		System.out.println("");
+
+	}
+
+}
---
+++ src/com/javaex/ex03/BookDao.java
@@ -0,0 +1,213 @@
+package com.javaex.ex03;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDao {
+
+	// 필드
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String id = "webdb";
+	private String pw = "webdb";
+
+	// DB연결
+	private void getConnection() {
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+			System.out.println("접속성공");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+	}
+
+	// 자원정리
+	private void close() {
+		// 5. 자원정리
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+	}
+
+	// Delete
+	public int bookDelete(int book_id) {
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " delete from book ";
+			query += " where book_id = ? ";
+
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, book_id);
+
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			System.out.println(count + "건 삭제");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		this.close();
+
+		return count;
+	}
+
+	// Update
+	public int bookUpdate(String title, String pubs, String pub_date, int book_id) {
+
+		int count = -1;
+
+		getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " update book ";
+			query += " set title = ?, ";
+			query += "     pubs = ?, ";
+			query += "     pub_date = ? ";
+			query += " where book_id = ? ";
+
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setString(1, title);
+			pstmt.setString(2, pubs);
+			pstmt.setString(3, pub_date);
+			pstmt.setInt(4, book_id);
+
+			count = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// 4.결과처리
+		System.out.println(count + "건 수정");
+
+		close();
+
+		return count;
+	}
+
+	// Insert
+	public int bookInsert(BookVo bookVo) {
+
+		int count = -1;
+
+		getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " insert into book ";
+			query += " values(seq_book_id.nextval, ?, ?, ?, ?) ";
+
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setString(1, bookVo.getTitle());
+			pstmt.setString(2, bookVo.getPubs());
+			pstmt.setString(3, bookVo.getPub_date());
+			pstmt.setInt(4, bookVo.getAuthor_id());
+
+			count = pstmt.executeUpdate();
+			
+			
+
+			// 4.결과처리
+			System.out.println(count + "건 등록");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		close();
+
+		return count; // 성공갯수 리턴
+	}
+	
+	// List
+	public List<BookVo> BookList() {
+		// DB값을 가져와서 ArrayList로 전달
+
+		// 리스트 생성
+		List<BookVo> bookList = new ArrayList<BookVo>();
+
+		
+		getConnection();
+		
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " select b.book_id, ";
+			query += "        b.title, ";
+			query += "        b.pubs, ";
+			query += "        to_char(b.pub_date,'yyyy-mm-dd'), ";
+			query += "        b.author_id, ";
+			query += "        a.author_name, ";
+			query += "        a.author_desc ";
+			query += " from author a, book b ";
+			query += " where a.author_id = b.author_id ";
+			query += " order by book_id asc";
+			
+			pstmt = conn.prepareStatement(query);
+			
+			rs = pstmt.executeQuery();
+			
+			// 4.결과처리
+			while (rs.next()) {
+				int book_id = rs.getInt("book_id");
+				String title = rs.getString("title");
+				String pubs = rs.getString("pubs");
+				String pub_date = rs.getString("to_char(b.pub_date,'yyyy-mm-dd')");
+				int author_id = rs.getInt("author_id");
+				String author_name = rs.getString("author_name");
+				String author_desc = rs.getString("author_desc");
+
+				BookVo bookVo = new BookVo(book_id, title, pubs, pub_date, author_id, author_name, author_desc);
+
+				bookList.add(bookVo);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		close();
+
+		return bookList;
+
+	}
+
+}
